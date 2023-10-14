a9f27a5ed120f5c5acc8e0d1250f3f23bdc17f73
Sandhya <stefanus.sandhya@gmail.com>
2023-10-14 20:03:25 +0700

    baru

--- main.go
+++ main.go
@@ -17,17 +17,17 @@ func hitungTip(tagihan float64) float64 {
 }
 
 func main() {
-	// Nilai tagihan
+	// Data nilai tagihan yang ingin di coba
 	tagihan1 := 275.0
 	tagihan2 := 430.0
 	tagihan3 := 80.00
 
-	// Menghitung tip
+	// Menghitung tip untuk tagihan
 	tip1 := hitungTip(tagihan1)
 	tip2 := hitungTip(tagihan2)
 	tip3 := hitungTip(tagihan3)
 
-	// Menampilkan informasi
+	// Menampilkan infromasi perhitungan tagihan, tip, dan total nilai
 	tampilkanInfo := func(tagihan float64, tip float64) {
 		total := tagihan + tip
 		fmt.Printf("Tagihannya %.2f, tipnya %.2f, dan total nilainya %.2f\n", tagihan, tip, total)
